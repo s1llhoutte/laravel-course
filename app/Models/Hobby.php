ef666e9aa5b853bf9a25d97e5326d669b86a6573
s1llhoutte <danikval2000002@gmail.com>
2023-08-07 12:50:24 +0300

    read model's

--- app/Models/Hobby.php
+++ app/Models/Hobby.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Hobby extends Model
 {
     use HasFactory;
+
+    protected $table = 'hobbies';
 }
--- database/migrations/2023_08_03_111939_create_pets_table.php
+++ database/migrations/2023_08_03_111939_create_pets_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('name');
             $table->string('image')->nullable(true);
+            $table->unsignedBigInteger('likes');
             $table->boolean('is_published')->default(1);
             $table->timestamps();
         });
