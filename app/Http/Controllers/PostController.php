077fdb70441acfdbc3e4ad019c3390d909482500
s1llhoutte <danikval2000002@gmail.com>
2023-08-07 13:52:11 +0300

    read/retrieve

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,23 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-        $post = Post::find(1);
-        dd($post->likes);
+        // выбрать все и вывести все значения тайтла
+        $posts = Post::all();
+        foreach ($posts as $post) {
+           dump($post->title);
+        }
+        dump('end 1 stolbik');
+
+        // или выбрать только те что имеют 1 в столбике is_published
+        $posts2 = Post::where('is_published', 1)->get();
+        foreach ($posts2 as $post2) {
+            dump($post2->title);
+        }
+        dump('end 2 stolbik');
+
+        // или выбрать только первую линию что имеют 1 в столбике is_published
+        $posts3 = Post::where('is_published', 1)->first();
+        dump($posts3->title);
+        dump('end video yrok');
     }
 }
