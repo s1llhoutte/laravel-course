ece9ba94ea8c11a3466fcf2b12d03622a01fad64
s1llhoutte <danikval2000002@gmail.com>
2023-08-08 15:39:09 +0300

    view pt.1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,24 +8,9 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-        // выбрать все и вывести все значения тайтла
         $posts = Post::all();
-        foreach ($posts as $post) {
-           dump($post->title);
-        }
-        dump('end 1 stolbik');
 
-        // или выбрать только те что имеют 1 в столбике is_published
-        $posts2 = Post::where('is_published', 1)->get();
-        foreach ($posts2 as $post2) {
-            dump($post2->title);
-        }
-        dump('end 2 stolbik');
-
-        // или выбрать только первую линию что имеют 1 в столбике is_published
-        $posts3 = Post::where('is_published', 1)->first();
-        dump($posts3->title);
-        dump('end video yrok');
+        return view('posts', compact('posts'));
     }
 
     public function create() {
