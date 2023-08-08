fb2c6c02b96fbe99dc2999226f97720ec44413e5
s1llhoutte <danikval2000002@gmail.com>
2023-08-08 16:47:28 +0300

    bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,18 +6,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{ route('main.index') }}">Main</a></li>
-                <li><a href="{{ route('post.index') }}">Posts</a></li>
-                <li><a href="{{ route('about.index') }}">About</a></li>
-                <li><a href="{{ route('contact.index') }}">Contacts</a></li>
-            </ul>
-        </nav>
+    <div class="container">
+        <div class='row'>
+            <nav>
+                <ul>
+                    <li><a href="{{ route('main.index') }}">Main</a></li>
+                    <li><a href="{{ route('post.index') }}">Posts</a></li>
+                    <li><a href="{{ route('about.index') }}">About</a></li>
+                    <li><a href="{{ route('contact.index') }}">Contacts</a></li>
+                </ul>
+            </nav>
+        </div>
+        @yield('content')
     </div>
-    @yield('content')
 </body>
 </html>
