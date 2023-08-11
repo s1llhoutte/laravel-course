44c087ec1f4a0a2ad44f1f63e41fbe7cce2c6212
s1llhoutte <danikval2000002@gmail.com>
2023-08-11 14:31:14 +0300

    crud interfaces pt.3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -47,8 +47,9 @@ class PostController extends Controller
 
     public function edit(Post $post) {
         $categories = Category::all();
+        $tags = Tag::all();
 
-        return view('post.edit', compact('post', 'categories'));
+        return view('post.edit', compact('post', 'categories', 'tags'));
     }
 
     public function update(Post $post) {
@@ -57,8 +58,13 @@ class PostController extends Controller
             'content' => 'string',
             'image' => 'string',
             'category_id' => '',
+            'tags' => '',
         ]);
+        $tags = $data['tags'];
+        unset($data['tags']);
+
         $post->update($data);
+        $post->tags()->sync($tags);
         return redirect()->route('post.show', $post->id);
     }
 
