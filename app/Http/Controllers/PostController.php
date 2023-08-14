c5f019a5abf67cf45ae24f16991e8bd39dbe32ff
s1llhoutte <danikval2000002@gmail.com>
2023-08-14 17:47:11 +0300

    odin ko ... v laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
-use App\Models\PostTag;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
-        $posts = Post::all();
+        $post = Post::find(1);
+        $category = Category::find(1);
+        $tag = Tag::find(1);
+        dd($post->tags);
         return view('post.index', compact('posts'));
     }
 
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,22 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Symfony\Polyfill\Ctype\Ctype;
 
 class Post extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
-    protected $table = 'posts';
-
-    protected $guarded = [];
-
-    public function category() {
-        return $this->belongsTo(Category::class, 'category_id', 'id');
+    public function category(){
+        return $this->belongsTo(Category::class);
     }
 
     public function tags() {
-        return $this->belongsToMany(Tag::class, 'post_tags', 'post_id', 'tag_id');
+        return $this->belongsToMany(Tag::class);
     }
 }
