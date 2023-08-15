c29ce5fada7cbdadfe92eb519ed700015ff2b6fc
s1llhoutte <danikval2000002@gmail.com>
2023-08-15 15:28:48 +0300

    factory i seed v laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Category::factory(20)->create();
+        $tags = Tag::factory(50)->create();
+        $posts = Post::factory(200)->create();
+
+        foreach ($posts as $post) {
+            $tagsIds = $tags->random(5)->pluck('id');
+            $post->tags()->attach($tagsIds);
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
