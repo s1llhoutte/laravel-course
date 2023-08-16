cafe1ff8e9017627b102846785ccbfea80751622
s1llhoutte <danikval2000002@gmail.com>
2023-08-16 17:51:57 +0300

    class Policy

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -22,6 +22,11 @@
                             <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
                             <a class="nav-link" href="{{ route('about.index') }}">About</a>
                             <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+
+                            @can('view', auth()->user())
+                                <a class="nav-link" href="{{ route('post.index') }}">Admin</a>
+                            @endcan
+
                         </div>
                     </div>
                 </div>
