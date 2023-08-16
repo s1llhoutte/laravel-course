c4166696c8f54829d248ba510f1cfadad9670f33
s1llhoutte <danikval2000002@gmail.com>
2023-08-16 15:52:47 +0300

    autorisation laravel

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'aaaaaaaaa';
-});
+Route::get('/', 'App\Http\Controllers\HomeController@index');
 
 
 Route::group(['namespace' => 'App\Http\Controllers\Post'], function(){
@@ -42,3 +40,7 @@ Route::get('/posts/update_or_create', 'App\Http\Controllers\PostController@updat
 Route::get('/main', 'App\Http\Controllers\MainController@index')->name('main.index');
 Route::get('/contacts', 'App\Http\Controllers\ContactController@index')->name('contact.index');
 Route::get('/about', 'App\Http\Controllers\AboutController@index')->name('about.index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
